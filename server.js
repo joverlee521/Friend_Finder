f7a62c184626ae28e8810c5214e229f98e91a6a4
J.Lee <joverlee521@gmail.com>
2018-11-01 18:10:23 -0700

    basic route sends user to home page

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ var path = require("path");
 
 var app = express();
 var PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req,res){
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.listen(PORT, function(){
+    console.log("App listening on PORT: " + PORT);
+});
