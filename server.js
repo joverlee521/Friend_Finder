3d106f5f728fe099db9c7b48ae52b0d37e5b1cb4
J.Lee <joverlee521@gmail.com>
2018-11-01 18:17:53 -0700

    routed survey page

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.get("/", function(req,res){
     res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 });
