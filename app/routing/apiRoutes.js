bad8046633c6e692f96b5c50a0feed15897c2f72
J.Lee <joverlee521@gmail.com>
2018-11-05 15:24:35 -0800

    added api get and post routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,11 +1,15 @@
 var express = require("express");
-var path = require("path");
 var friends = require("../data/friends");
-
 var api = express.Router();
 
 api.get("/api/friends", function(req, res){
-    return res.json(friends);
+    res.json(friends);
+});
+
+api.post("/api/friends", function(req, res){
+    var newFriends = req.body;
+    friends.push(newFriends);
+    res.json(newFriends);
 });
 
 module.exports = api;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", home);
-app.use("/api/friends", api)
+app.use("/", api);
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
