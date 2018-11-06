adb8a0b6d8d95e649c9ba017554ed39dfc78dc37
J.Lee <joverlee521@gmail.com>
2018-11-06 11:32:22 -0800

    finished homepage and survey page

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,8 @@
 var friends = [
     {
-        name: "Jover",
-        photo: "photo"
+        "name" : "Jover",
+        "photo" : "https://avatars0.githubusercontent.com/u/40774762?s=460&v=4",
+        "scores" : []
     }
 ]
 
