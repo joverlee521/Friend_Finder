758c85a35c85b300e9973267016838cba76ee066
J.Lee <joverlee521@gmail.com>
2018-11-06 20:24:04 -0800

    added comments

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+// Required NPM package
 var express = require("express");
-
+// Store express function in a variable
 var app = express();
+// Dynamic PORT that can be used with Heroku or locally
 var PORT = process.env.PORT || 8080;
 
+// Express middleware to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Link server to route files
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
+// Starts the server
 app.listen(PORT, function(){
     console.log("App listening on PORT: " + PORT);
 });
