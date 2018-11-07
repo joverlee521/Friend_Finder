57ed08d08451e281dea0ec7cd04ce6f7592f24eb
J.Lee <joverlee521@gmail.com>
2018-11-06 21:00:14 -0800

    added comments

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,12 @@
+// Required NPM package
 var path = require("path");
 
 module.exports = function(app){
-
+    // HTML GET requests to display HTML content when user visits the page
     app.get("/survey", function(req, res){
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
-
+    // If not matching route if found, the website will default to the homepage
     app.get("*", function(req, res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     })
